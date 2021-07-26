ee45aedb00b4388b8feab077fabae126d032a474
Artiom Giza <artiomg@gett.com>
2021-07-27 01:11:06 +0300

    add router + dumy meetups wiith styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritePage from "./pages/Favorites";
+
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritePage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
