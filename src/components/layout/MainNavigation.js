6567a3c8b32dfded7c554ef3bd5a938942c490ba
Artiom Giza <artiomg@gett.com>
2021-08-01 00:06:35 +0300

    use context - final commit

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import FavoritesContext from "../../store/favorites-context";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
+const favoritexCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,9 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">My Favorites
+            <span className={classes.badge}>{favoritexCtx.totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
